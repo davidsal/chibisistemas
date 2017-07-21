d6ba556df856e8d5909c3f1c6ac0548a731e9f29
David Melara <davidsal1995@gmail.com>
2017-07-21 00:04:18 -0600

    Add files via upload

---
+++ Librerias/Python3/ChibiBases/Ejemplo.py
@@ -0,0 +1,7 @@
+import ChibiBases  # Importamos la libreria
+
+print("Base32 6I es: " + str(ChibiBases.convertirbaseadecimal("6I", 32)))  # La funcion recibe un string y regresa un entero decimal
+
+print("124 en Octal: " + ChibiBases.convertirdecimalabase(124, 8))  # La funcion recibe un entero decimal y devuelve un string convertido a nueva base
+
+print("174 octal a Decimal: " + str(ChibiBases.convertirbaseadecimal("174", 8)))
